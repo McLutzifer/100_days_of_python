5cd46e4753a1155fef3aaaa4af2858983a8c2130
McLutzifer <lukas.iselor@tutanota.com>
2022-04-13 20:55:22 +0200

    remove itertools

---
+++ Day_001/band_name_generator.py
@@ -0,0 +1,8 @@
+print('Hello')
+
+city = input("What is the city you grew up in?\n > ")
+pet = input("What's the name of your pet?\n > ")
+
+bandname = city + " " + pet
+
+print(f"your bandname is >>{bandname}<<")
