350eef1318bb28e6942929be2134aca27913de2c
McLutzifer <lukas.iselor@tutanota.com>
2022-04-14 19:30:32 +0200

    style: remove unnecessary comments

--- Day_017/quiz-game-start/main.py
+++ Day_017/quiz-game-start/main.py
@@ -4,7 +4,7 @@ from quiz_brain import QuizBrain
 
 question_bank = []
 for element in question_data:
-    question_bank.append(Question(element["question"], element["correct_answer"]))  # FIX
+    question_bank.append(Question(element["question"], element["correct_answer"]))
 
 quiz = QuizBrain(question_bank)
 while quiz.still_has_questions():
