01feea5455fe20e42c53da23b331f77efbfe6f1e
McLutzifer <lukas.iselor@tutanota.com>
2022-04-15 20:09:28 +0200

    feat: creat main.py for day 18 project

---
+++ Day_018/main.py
@@ -0,0 +1,21 @@
+from turtle import Screen, Turtle
+
+timmy_the_turtle = Turtle()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("DarkGoldenrod")
+timmy_the_turtle.forward(180)
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(180)
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
