77ef9121940be62831bba45f215b4635197119c9
McLutzifer <lukas.iselor@tutanota.com>
2022-04-16 15:20:28 +0200

    feat: add random path and pensize

--- Day_018/challenge_4.py
+++ Day_018/challenge_4.py
@@ -13,7 +13,17 @@ colors = [
     "light salmon"
 ]
 
+directions = [0, 90, 180, 270]
 
+turtle = Turtle()
+turtle.pensize(10)
+turtle.shape("turtle")
+turtle.speed(6)
+
+for _ in range(200):
+    turtle.forward(30)
+    turtle.right(random.choice(directions))
+    turtle.color(random.choice(colors))
 
 
 screen = Screen()
