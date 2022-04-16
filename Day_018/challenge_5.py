e4085f103b65e7630b4d9ee7e828983e7292b632
McLutzifer <lukas.iselor@tutanota.com>
2022-04-16 16:21:15 +0200

    style: refactor according to PEP8

--- Day_018/challenge_5.py
+++ Day_018/challenge_5.py
@@ -5,13 +5,14 @@ my_turtle = turtle.Turtle()
 turtle.colormode(255)
 my_turtle.speed("fastest")
 
+
 def random_color():
     r = random.randint(0, 255)
     g = random.randint(0, 255)
     b = random.randint(0, 255)
+    color = (r, g, b)
+    return color
 
-    random_color = (r, g, b)
-    return random_color
 
 number_of_circles = 100
 
