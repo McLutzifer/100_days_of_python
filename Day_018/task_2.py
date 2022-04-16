85b6017932ad5bce4ec7bd33cbdc48ed566857cb
McLutzifer <lukas.iselor@tutanota.com>
2022-04-16 15:06:41 +0200

    refactor: clean up code ad remove comments

--- Day_018/task_2.py
+++ Day_018/task_2.py
@@ -6,23 +6,11 @@ turtle.shape("turtle")
 
 colors  = ["red","green","blue","orange","purple","pink","yellow"]
 
-# def change_color():
-#     R = random.randrange(0, 257, 10)
-#     G = random.randrange(0, 257, 10)
-#     B = random.randrange(0, 257, 10)
-
-#     return R, G, B
-
-
-for i in range (4, 19):
+for i in range (3, 11):
     for j in range (i):
         turtle.forward(100)
         turtle.right(360/i)
-    color = random.choice(colors)
-    #r, g, b = change_color()
-    #turtle.color(r, g, b)
-    turtle.color(color)
-
+    turtle.color(random.choice(colors))
 
 
 screen = Screen()
