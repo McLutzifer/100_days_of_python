3d59b4c37c4c5a9e9883622600d24c46bf335d08
McLutzifer <lukas.iselor@tutanota.com>
2022-04-16 15:00:02 +0200

    tutorial: follow along video # 168

--- Day_018/main.py
+++ Day_018/main.py
@@ -1,20 +1,14 @@
 from turtle import Screen, Turtle
 
-timmy_the_turtle = Turtle()
-timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("DarkGoldenrod")
-timmy_the_turtle.forward(180)
-timmy_the_turtle.right(90)
-timmy_the_turtle.forward(180)
+tim = Turtle()
+tim.shape("turtle")
+tim.color("DarkGoldenrod")
 
-
-
-
-
-
-
-
-
+for _ in range(15):
+    tim.pendown()
+    tim.forward(10)
+    tim.penup()
+    tim.forward(10)
 
 
 screen = Screen()
