ba7f4a3a1a90120be4924975d3866fbe37cc3ab7
McLutzifer <lukas.iselor@tutanota.com>
2022-04-16 15:16:37 +0200

    feat: add color options

---
+++ Day_018/challenge_4.py
@@ -0,0 +1,20 @@
+from secrets import choice
+from turtle import Turtle, Screen
+import random
+
+colors = [
+    "pale green",
+    "dark turquoise",
+    "light slate gray",
+    "plum",
+    "blanched almond",
+    "sky blue",
+    "light gray",
+    "light salmon"
+]
+
+
+
+
+screen = Screen()
+screen.exitonclick()
