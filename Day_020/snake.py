a4568847c4f45e6f8f382bd2b54968b6aa8725ca
McLutzifer <lukas.iselor@tutanota.com>
2022-04-18 03:47:48 +0200

    feat: add move logic

--- Day_020/snake.py
+++ Day_020/snake.py
@@ -1,12 +1,21 @@
 from turtle import Turtle
 
-class Snake(): 
+STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+class Snake: 
 
     def __init__(self) -> None:
-        starting_postion = [(0, 0), (-20, 0), (-40, 0)]
-        self.self.segments = []
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
 
-        for position in starting_postion:
+    def create_snake(self) -> None:
+        for position in STARTING_POSITION:
             new_segment = Turtle("square")
             new_segment.color("white")
             new_segment.penup()
@@ -14,11 +23,26 @@ class Snake():
             self.segments.append(new_segment)
 
 
-    def move(self):
-        # move snake
-        for seg_num in range(len(self.self.segments)-1, 0, -1):
+    def move(self) -> None:
+        for seg_num in range(len(self.segments)-1, 0, -1):
             new_x = self.segments[seg_num -1].xcor()
             new_y = self.segments[seg_num -1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-        self.segments[0].forward(20)
-  
+        self.head.forward(MOVE_DISTANCE)
+    
+
+    def up(self) -> None:
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+    
+    def down(self) -> None:
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self) -> None:
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+    
+    def right(self) -> None:
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
