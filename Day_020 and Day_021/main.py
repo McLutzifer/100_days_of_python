af0c3723a677642435e9f04a295adad46aa63987
McLutzifer <lukas.iselor@tutanota.com>
2022-04-20 16:53:13 +0200

    feat: create new food at random place at collision

--- Day_020 and Day_021/main.py
+++ Day_020 and Day_021/main.py
@@ -1,6 +1,7 @@
 import time
-from turtle import Screen, Turtle
+from turtle import Screen
 from snake import Snake
+from food import Food
 
 screen =Screen()
 screen.setup(width=600, height=600)
@@ -9,7 +10,7 @@ screen.title("my Snake Game")
 screen.tracer(0)
 
 snake = Snake()
-
+food = Food()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -23,7 +24,9 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
-
+    #    detect_collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 
 
