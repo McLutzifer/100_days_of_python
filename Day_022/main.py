2e5b6e7f637b02638dae905a10ba9d3982f24f78
McLutzifer <lukas.iselor@tutanota.com>
2022-04-21 04:27:48 +0200

    feat: create screen, paddels and ball objects

---
+++ Day_022/main.py
@@ -0,0 +1,28 @@
+from turtle import Turtle, Screen
+from paddle import Paddle
+from ball import Ball
+
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("pong")
+screen.tracer(0)
+
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+
+screen.exitonclick()
