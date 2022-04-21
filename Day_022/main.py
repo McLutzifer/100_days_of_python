5f93ec31ab0ac2ca9a04afd0ae3b43ba099cb5db
McLutzifer <lukas.iselor@tutanota.com>
2022-04-21 16:53:41 +0200

    feat: add logic to detect different kind of  collisions

--- Day_022/main.py
+++ Day_022/main.py
@@ -27,4 +27,20 @@ while game_is_on:
     screen.update()
     ball.move()
 
+    # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect collision with r_paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.ycor() < -320:
+        ball.bounce_x()
+
+    # Detect R paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+
+    # Detect L paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+
 screen.exitonclick()
