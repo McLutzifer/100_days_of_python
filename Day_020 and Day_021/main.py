f7bb8c91700f401cef23a0724960e8368502dc4b
McLutzifer <lukas.iselor@tutanota.com>
2022-04-21 03:43:24 +0200

    feat: add functionality to detect game over

--- Day_020 and Day_021/main.py
+++ Day_020 and Day_021/main.py
@@ -1,3 +1,4 @@
+from telnetlib import GA
 import time
 from turtle import Screen
 from snake import Snake
@@ -27,11 +28,24 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
-    #    detect_collision with food
+    # Detect_collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
         scoreboard.increase_score()
 
+    # Detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.ycor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+    
+    # Detect collision with tail
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 
 
