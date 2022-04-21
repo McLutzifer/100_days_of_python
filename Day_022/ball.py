a69ef2c7c991c7cb71abaf2d3b9deeac25021c4b
McLutzifer <lukas.iselor@tutanota.com>
2022-04-21 16:30:27 +0200

    feat: initialize ball object

--- Day_022/ball.py
+++ Day_022/ball.py
@@ -1,5 +1,6 @@
 from turtle import Turtle
 
+
 class Ball(Turtle):
 
     def __init__(self) -> None:
@@ -8,3 +9,8 @@ class Ball(Turtle):
         self.color("white")
         self.shape("square")
         self.penup()
+
+    def move(self):
+        new_x = self.xcor() + 10
+        new_y = self.ycor() + 10
+        self.goto(new_x, new_y)
