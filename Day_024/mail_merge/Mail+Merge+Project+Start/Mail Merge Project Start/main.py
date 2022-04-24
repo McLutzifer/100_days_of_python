1c8d04b7d68872ec92321bdda1c6a3e17060f786
McLutzifer <lukas.iselor@tutanota.com>
2022-04-24 16:53:24 +0200

    feat: add read and write method to main to finish challenge for video #224

--- Day_024/mail_merge/Mail+Merge+Project+Start/Mail Merge Project Start/main.py
+++ Day_024/mail_merge/Mail+Merge+Project+Start/Mail Merge Project Start/main.py
@@ -1,23 +1,22 @@
-
+PLACEHOLDER = "[name]"
 # in Ready to send leter_for_[name].txt
 
 # import names
 all_names = []
 
-with open("/home/lukas/Documents/GitLab/100_days_of_python/Day_024/mail_merge/Mail+Merge+Project+Start/Mail Merge Project Start/Input/Names/invited_names.txt") as data:
-    names = data.readlines()
+with open("./Input/Names/invited_names.txt") as names_file:
+    names = names_file.readlines()
     for name in names:
         name = name.strip("\n")
         all_names.append(name)
 
 # import starting starting_letter
-with open("/home/lukas/Documents/GitLab/100_days_of_python/Day_024/mail_merge/Mail+Merge+Project+Start/Mail Merge Project Start/Input/Letters/starting_letter.txt") as starting_letter:
-    test = starting_letter.readlines()
-    print(test[0])
+with open("./Input/Letters/starting_letter.txt") as starting_letter:
+    letter = starting_letter.read()
+    print(letter[0])
 
-
-
-
-
-
-print(all_names)
+    for name in all_names:
+        personal_letter = letter.replace(PLACEHOLDER, name)
+        
+        with open(f"./Output/ReadyToSend/letter_for_{name}", mode="w") as completed_letter:
+            completed_letter.write(personal_letter)
