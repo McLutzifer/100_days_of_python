bcc7e0671aba5514b407df7a09f3549b52bc7caf
McLutzifer <lukas.iselor@tutanota.com>
2022-04-25 17:12:56 +0200

    feat: solve chellenge for video #229

--- Day_025/main.py
+++ Day_025/main.py
@@ -1,7 +1,17 @@
 import pandas as pd
 
-print("test")
+squirrel_data = pd.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
 
-data = pd.read_csv("weather_data.csv")
+gray_squirrels = len(squirrel_data[squirrel_data["Primary Fur Color"] == "Gray"])
+black_squirrels = len(squirrel_data[squirrel_data["Primary Fur Color"] == "Black"])
+red_squirrels = len(squirrel_data[squirrel_data["Primary Fur Color"] == "Cinnamon"])
 
-print(data["temp"])
+headings = ["Fur Color", "Count"]
+
+squirrel_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [gray_squirrels, black_squirrels, red_squirrels]
+}
+
+squirrel_count = pd.DataFrame(squirrel_dict)
+squirrel_count.to_csv("squirrel_count.csv")
